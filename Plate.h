a8133a3143e7d54e5592b100f5bf4fd16095a4dc
kae1122 <kae_314@icloud.com>
2021-07-08 17:15:36 +0900

    first commit

---
+++ Plate.h
@@ -0,0 +1,15 @@
+#ifndef Plate_H
+#define Plate_H
+
+#include<iostream>
+
+class Plate {
+public:
+    int size;
+
+    Plate(int sz);
+    Plate(const Plate& plate1);
+    void setplate(int x);
+};
+
+#endif /*Plate_H_*/"# hanoi" 
